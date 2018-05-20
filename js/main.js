c373c6597be4251ee44270b8139ebffa4aaf07a4
Martin Kravchenko <martin@martinonweb.com>
2018-05-20 10:45:38 +0200

    ESLint

--- js/main.js
+++ js/main.js
@@ -1,111 +1,111 @@
 // Creating colors
-var numOfColors = 6;
-var colors = generateColors(numOfColors);
+var numOfColors = 6
+var colors = generateColors(numOfColors)
 
 // Initializing
-var goalColor = colors[pickColor()];
-var squares = document.querySelectorAll(".square");
-var colorGoalDisplay = document.getElementById("colorGoalDisplay");
-var h1 = document.querySelector("h1");
-var resetB = document.querySelector("#reset");
-var easyB = document.querySelector("#easy");
-var hardB = document.querySelector("#hard");
-var ultraB = document.querySelector("#ultra");
-var container = document.querySelector("#container");
+var goalColor = colors[pickColor()]
+var squares = document.querySelectorAll(".square")
+var colorGoalDisplay = document.getElementById("colorGoalDisplay")
+var h1 = document.querySelector("h1")
+var resetB = document.querySelector("#reset")
+var easyB = document.querySelector("#easy")
+var hardB = document.querySelector("#hard")
+var ultraB = document.querySelector("#ultra")
+var container = document.querySelector("#container")
 
-setSquares();
+setSquares()
 
 easyB.addEventListener("click", function () {
 	if (numOfColors != 3) {
-		easyB.classList.add("selected");
-		hardB.classList.remove("selected");
-		ultraB.classList.remove("selected");
-		container.innerHTML = "<div class='square'></div><div class='square'></div><div class='square'></div>";
-		squares = document.querySelectorAll(".square");
-		numOfColors = 3;
-		setSquares();
+		easyB.classList.add("selected")
+		hardB.classList.remove("selected")
+		ultraB.classList.remove("selected")
+		container.innerHTML = "<div class='square'></div><div class='square'></div><div class='square'></div>"
+		squares = document.querySelectorAll(".square")
+		numOfColors = 3
+		setSquares()
 	}
-});
+})
 
 hardB.addEventListener("click", function () {
 	if (numOfColors != 6) {
-		hardB.classList.add("selected");
-		easyB.classList.remove("selected");
-		ultraB.classList.remove("selected");
-		container.innerHTML = "<div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div>";
-		squares = document.querySelectorAll(".square");
-		numOfColors = 6;
-		setSquares();
+		hardB.classList.add("selected")
+		easyB.classList.remove("selected")
+		ultraB.classList.remove("selected")
+		container.innerHTML = "<div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div>"
+		squares = document.querySelectorAll(".square")
+		numOfColors = 6
+		setSquares()
 	}
-});
+})
 
 ultraB.addEventListener("click", function () {
 	if (numOfColors != 9) {
-		ultraB.classList.add("selected");
-		hardB.classList.remove("selected");
-		easyB.classList.remove("selected");
-		container.innerHTML = "<div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div>";
-		squares = document.querySelectorAll(".square");
-		numOfColors = 9;
-		setSquares();
+		ultraB.classList.add("selected")
+		hardB.classList.remove("selected")
+		easyB.classList.remove("selected")
+		container.innerHTML = "<div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div>"
+		squares = document.querySelectorAll(".square")
+		numOfColors = 9
+		setSquares()
 	}
-});
+})
 
 resetB.addEventListener("click", function () {
-	setSquares();
-});
+	setSquares()
+})
 
 function setSquares() {
-	colors = generateColors(numOfColors);
-	goalColor = colors[pickColor()];
+	colors = generateColors(numOfColors)
+	goalColor = colors[pickColor()]
 	// Displaying the goal color
-	colorGoalDisplay.textContent = goalColor;
+	colorGoalDisplay.textContent = goalColor
 
 	// Looping through the squares
 	for (var i = 0; i < squares.length; i++) {
 		// Setting squares' initial color
-		squares[i].style.backgroundColor = colors[i];
+		squares[i].style.backgroundColor = colors[i]
 
 		// Adding listeners
 		squares[i].addEventListener("click", function () {
 			// Grab color of clicked square
-			var clickedColor = this.style.backgroundColor;
+			var clickedColor = this.style.backgroundColor
 			// Compare grabbed color to the goal
 			if (clickedColor === goalColor) {
-				changeColors(goalColor);
-				h1.style.backgroundColor = goalColor;
-				resetB.textContent = "Play Again?";
+				changeColors(goalColor)
+				h1.style.backgroundColor = goalColor
+				resetB.textContent = "Play Again?"
 			} else {
-				this.style.backgroundColor = "#232323";
+				this.style.backgroundColor = "#232323"
 			}
-		});
+		})
 	}
 
-	h1.style.backgroundColor = "steelblue";
-	resetB.textContent = "New Colors";
+	h1.style.backgroundColor = "steelblue"
+	resetB.textContent = "New Colors"
 }
 
 function changeColors(color) {
 	for (var i = 0; i < squares.length; i++) {
-		squares[i].style.backgroundColor = color;
+		squares[i].style.backgroundColor = color
 	}
 }
 
 function pickColor() {
-	return Math.floor(Math.random() * colors.length);
+	return Math.floor(Math.random() * colors.length)
 }
 
 function generateColors(num) {
-	var arr = [];
+	var arr = []
 	for (var i = 0; i < num; i++) {
-		arr.push(randomColor());
+		arr.push(randomColor())
 	}
-	return arr;
+	return arr
 }
 
 function randomColor() {
-	var R = Math.floor(Math.random() * 256);
-	var G = Math.floor(Math.random() * 256);
-	var B = Math.floor(Math.random() * 256);
-	return "rgb(" + R + ", " + G + ", " + B + ")";
+	var R = Math.floor(Math.random() * 256)
+	var G = Math.floor(Math.random() * 256)
+	var B = Math.floor(Math.random() * 256)
+	return "rgb(" + R + ", " + G + ", " + B + ")"
 }
